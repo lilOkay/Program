ef32b4ccbcd02a68a1bf52b038c1225281e787fa
lilOkay <gncokay@gmail.com>
2021-01-16 23:48:29 +0300

    exercise

---
+++ Program/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2};
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine("-----------");
+            }
+            Console.WriteLine("-------metotlar--------");
+            //encapsulation//
+            SepetManager sepetmanager = new SepetManager();
+            sepetmanager.Ekle(urun1);
+            sepetmanager.Ekle(urun2);
+            sepetmanager.Ekle2("armut", "yesil Armut", 12,10) ;
+            sepetmanager.Ekle2("Elma", "yesil Elma", 12,9);
+            sepetmanager.Ekle2("Karpuz", "yesil Karpuz", 12,8);
+
+        }
+    }
+}
---
+++ Program/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    class SepetManager
+    {
+       public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler .Sepete Eklendi! : " +urun.Adi);
+        }
+        public void Ekle2(string urunadi,string aciklama, double fiyati, int StockAdedi)
+        {
+            Console.WriteLine("Tebrikler sepete eklendi! : " + urunadi);
+        }
+    }
+}
